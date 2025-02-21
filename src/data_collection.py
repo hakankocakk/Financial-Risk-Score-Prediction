29c510be829744e16bb8816b2cbe3a0fec827a78
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2025-02-22 00:50:00 +0300

    update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -16,7 +16,7 @@ def data_colletion(dataframe):
 data = pd.read_csv(os.path.join(os.path.dirname(__file__), "financial_risk_scores.csv"))
 train_data, validation_data, test_data = data_colletion(data)
 
-data_path = os.path.join(os.path.dirname(__file__), "data", "raw")
+data_path = os.path.join(os.path.dirname(__file__), "..", "data", "raw")
 os.makedirs(data_path)
 
 train_data.to_csv(os.path.join(data_path, "train.csv"), index=False)
