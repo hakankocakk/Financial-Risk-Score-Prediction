c2a51cce9512336bc3d6ac4d39ed1d2d6489781d
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2025-02-22 16:14:16 +0300

    update data_preprocess.py

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -5,13 +5,6 @@ from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler,
 import joblib
 
 
-data_path_input = os.path.join(os.path.dirname(__file__), "..", "data", "raw")
-data_path_output = os.path.join(os.path.dirname(__file__), "..", "data", "processed")
-model_path = os.path.join(os.path.dirname(__file__), "model", "process_model")
-
-os.makedirs(data_path_output)
-os.makedirs(model_path)
-
 
 def create_cols_types(dataframe, threshold_cat=8):
 
@@ -48,13 +41,14 @@ def ordinalencoding(dataframe, train=True):
         dataframe[columns_to_encode] = enc.fit_transform(dataframe[columns_to_encode])
         joblib.dump(enc, os.path.join(model_path, 'ordinal_encoder.pkl'))
     else:
+        model_path = os.path.join(os.path.dirname(__file__), "model", "process_model")
         loaded_encoder = joblib.load(os.path.join(model_path, 'ordinal_encoder.pkl'))
         dataframe[columns_to_encode] = loaded_encoder.transform(dataframe[columns_to_encode])
 
     return dataframe
 
 
-def onehotencoder(dataframe, train=True):
+def onehotencoding(dataframe, train=True):
 
     one_hot_cat_cols = ['EducationLevel', 'MaritalStatus', 'HomeOwnershipStatus', 'LoanPurpose', 'NumberOfDependents']
 
@@ -67,6 +61,7 @@ def onehotencoder(dataframe, train=True):
         dataframe = pd.concat([dataframe, encoded_df], axis=1)
         dataframe.drop(columns=one_hot_cat_cols, inplace=True)
     else:
+        model_path = os.path.join(os.path.dirname(__file__), "model", "process_model")
         loaded_ohe = joblib.load(os.path.join(model_path, 'one_hot_encoder.pkl'))
         encoded_test_data = loaded_ohe.transform(dataframe[one_hot_cat_cols])
         new_columns = loaded_ohe.get_feature_names_out(one_hot_cat_cols)
@@ -77,46 +72,61 @@ def onehotencoder(dataframe, train=True):
     return dataframe
 
 
-def normalization(dataframe, num_cols, train=True):
+def normalization(dataframe, train=True):
+    num_cols = ['Age', 'AnnualIncome', 'CreditScore', 'Experience', 'LoanAmount', 'LoanDuration', 'MonthlyDebtPayments', 
+                'CreditCardUtilizationRate', 'NumberOfOpenCreditLines', 'NumberOfCreditInquiries', 'DebtToIncomeRatio', 
+                'PaymentHistory', 'LengthOfCreditHistory', 'SavingsAccountBalance', 'CheckingAccountBalance', 'TotalAssets', 
+                'TotalLiabilities', 'MonthlyIncome', 'UtilityBillsPaymentHistory', 'JobTenure', 'NetWorth', 'BaseInterestRate', 
+                'InterestRate', 'MonthlyLoanPayment', 'TotalDebtToIncomeRatio', 'AnIncomeToAssetsRatio', 'AnExperienceToAnIncomeRatio', 
+                'LoantoAnIncomeRatio', 'DependetToAnIncomeRatio', 'LoansToAssetsRatio', 'LoanPaymentToIncomeRatio', 'AnIncomeToDepts', 
+                'AssetsToLoan']
 
     if train:
         scaler = StandardScaler()
         dataframe[num_cols] = scaler.fit_transform(dataframe[num_cols])
         joblib.dump(scaler, os.path.join(model_path, 'standardscaler.pkl'))
     else:
+        model_path = os.path.join(os.path.dirname(__file__), "model", "process_model")
         loaded_scaler = joblib.load(os.path.join(model_path, 'standardscaler.pkl'))
         dataframe[num_cols] = loaded_scaler.transform(dataframe[num_cols])
 
     return dataframe
 
 
+if __name__ == "__main__":
+
+    data_path_input = os.path.join(os.path.dirname(__file__), "..", "data", "raw")
+    data_path_output = os.path.join(os.path.dirname(__file__), "..", "data", "processed")
+    model_path = os.path.join(os.path.dirname(__file__), "model", "process_model")
+
+    os.makedirs(data_path_output, exist_ok=True)
+    os.makedirs(model_path, exist_ok=True)
+
+    train = pd.read_csv(os.path.join(data_path_input, "train.csv"))
+    validation = pd.read_csv(os.path.join(data_path_input, "validation.csv"))
+    test = pd.read_csv(os.path.join(data_path_input, "test.csv"))
+
+    cat_cols, num_cols = create_cols_types(train)
+
+    train = feature_engineering(train)
+    validation = feature_engineering(validation)
+    test = feature_engineering(test)
+
+    train = ordinalencoding(train, train=True)
+    validation = ordinalencoding(validation, train=False)
+    test = ordinalencoding(test, train=False)
+
+    train = onehotencoding(train, train=True)
+    validation = onehotencoding(validation, train=False)
+    test = onehotencoding(test, train=False)
+
+    #_, num_cols = create_cols_types(train)
+    #num_cols = [col for col in num_cols if col not in ["RiskScore"]]
+    train_data = normalization(train, train=True)
+    validation_data = normalization(validation, num_cols, train=False)
+    test_data = normalization(test, num_cols, train=False)
 
 
-train = pd.read_csv(os.path.join(data_path_input, "train.csv"))
-validation = pd.read_csv(os.path.join(data_path_input, "validation.csv"))
-test = pd.read_csv(os.path.join(data_path_input, "test.csv"))
-
-cat_cols, num_cols = create_cols_types(train)
-
-train = feature_engineering(train)
-validation = feature_engineering(validation)
-test = feature_engineering(test)
-
-train = ordinalencoding(train, train=True)
-validation = ordinalencoding(validation, train=False)
-test = ordinalencoding(test, train=False)
-
-train = onehotencoder(train, train=True)
-validation = onehotencoder(validation, train=False)
-test = onehotencoder(test, train=False)
-
-_, num_cols = create_cols_types(train)
-num_cols = [col for col in num_cols if col not in ["RiskScore"]]
-train_data = normalization(train, num_cols, train=True)
-validation_data = normalization(validation, num_cols, train=False)
-test_data = normalization(test, num_cols, train=False)
-
-
-train_data.to_csv(os.path.join(data_path_output, "train.csv"), index=False)
-validation_data.to_csv(os.path.join(data_path_output, "validation.csv"), index=False)
-test_data.to_csv(os.path.join(data_path_output, "test.csv"), index=False)
+    train_data.to_csv(os.path.join(data_path_output, "train.csv"), index=False)
+    validation_data.to_csv(os.path.join(data_path_output, "validation.csv"), index=False)
+    test_data.to_csv(os.path.join(data_path_output, "test.csv"), index=False)
