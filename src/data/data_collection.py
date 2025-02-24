23de7444dea508164d9c311d3d55ce9bd8fb4cca
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2025-02-25 00:08:18 +0300

    update data_colllection.py

--- src/data/data_collection.py
+++ src/data/data_collection.py
@@ -53,6 +53,7 @@ def main():
     yaml_path = os.path.join(os.path.dirname(__file__), "..", "..")
     raw_data_path = os.path.join(os.path.dirname(__file__), "..", "..","data", "raw", "financial_risk_scores.csv")
     interim_data_path = os.path.join(os.path.dirname(__file__), "..", "..","data", "interim")
+    print(interim_data_path)
 
     try:
         size_params = load_yaml(yaml_path)
