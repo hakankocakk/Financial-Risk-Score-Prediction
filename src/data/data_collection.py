23d62e076a6d264e1af6f0a12d26fc9e08da6c9b
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2025-03-09 23:45:22 +0300

    update data_collection.py : applied Flake8 style guide

--- src/data/data_collection.py
+++ src/data/data_collection.py
@@ -1,25 +1,24 @@
 import pandas as pd
-import numpy as np
 import os
 import yaml
 from sklearn.model_selection import train_test_split
 
 
-def load_data(path : str) -> pd.DataFrame:
+def load_data(path: str) -> pd.DataFrame:
     try:
         return pd.read_csv(path)
     except Exception as e:
         raise Exception(f"Error loading data from {path} : {e}")
 
 
-def load_yaml(yaml_path : str) -> dict:
+def load_yaml(yaml_path: str) -> dict:
     try:
         with open(os.path.join(yaml_path, 'params.yaml'), 'r') as file:
             size_params = yaml.safe_load(file)['data_collection']
             return size_params
     except Exception as e:
         raise Exception(f"Error loading paramaters from {yaml_path} : {e}")
-    
+
 
 def data_split(dataframe: pd.DataFrame, size_params: dict):
     try:
@@ -28,31 +27,38 @@ def data_split(dataframe: pd.DataFrame, size_params: dict):
         raise Exception(f"Feature not found: {e}")
     except Exception as e:
         raise Exception(f"Error while dropping features: {e}")
-    
+
     try:
-        train, temp = train_test_split(dataframe, test_size=size_params["val_test_size"], random_state=42)
-        validation, test = train_test_split(temp, test_size=size_params["test_size"], random_state=42)
+        train, temp = train_test_split(dataframe,
+                                       test_size=size_params["val_test_size"],
+                                       random_state=42)
+        validation, test = train_test_split(temp,
+                                            test_size=size_params["test_size"],
+                                            random_state=42)
         return train, validation, test
     except KeyError as e:
         raise Exception(f"Parameter key error: {e}")
     except Exception as e:
         raise Exception(f"Error during splitting: {e}")
-    
 
-def save_data(data: pd.DataFrame,  data_path : str) -> None:
+
+def save_data(data: pd.DataFrame,  data_path: str) -> None:
     try:
         data.to_csv(data_path, index=False)
     except KeyError as e:
         raise Exception(f"Dataframe not found : {e}")
     except Exception as e:
-        raise Exception(f"Error save data file")
+        raise Exception(f"Error save data file: {e}")
 
 
 def main():
 
     yaml_path = os.path.join(os.path.dirname(__file__), "..", "..")
-    raw_data_path = os.path.join(os.path.dirname(__file__), "..", "..","datas", "raw", "financial_risk_scores.csv")
-    interim_data_path = os.path.join(os.path.dirname(__file__), "..", "..","datas", "interim")
+    raw_data_path = os.path.join(os.path.dirname(__file__),
+                                 "..", "..", "datas", "raw",
+                                 "financial_risk_scores.csv")
+    interim_data_path = os.path.join(os.path.dirname(__file__),
+                                     "..", "..", "datas", "interim")
     os.makedirs(interim_data_path)
 
     try:
@@ -62,7 +68,8 @@ def main():
         train_data, validation_data, test_data = data_split(data, size_params)
 
         save_data(train_data, os.path.join(interim_data_path, "train.csv"))
-        save_data(validation_data, os.path.join(interim_data_path, "validation.csv"))
+        save_data(validation_data, os.path.join(interim_data_path,
+                                                "validation.csv"))
         save_data(test_data, os.path.join(interim_data_path, "test.csv"))
     except Exception as e:
         raise Exception(f"An error occured: {e}")
