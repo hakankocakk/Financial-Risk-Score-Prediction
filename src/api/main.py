ad47fcd5e7ed889ca1a08f7f88e466efb0a204bc
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2025-03-10 23:37:07 +0300

    update src/api/main.py : applied Flake8 style guide

--- src/api/main.py
+++ src/api/main.py
@@ -7,47 +7,61 @@ from src.data.data_model import FinancialInformation
 import src.features.data_preprocess as dp
 
 
-
 app = FastAPI(
     title="Financial Risk Score Prediction",
     description="Predicting Financial Risk Score"
 )
 
-def data(FinancialInformation : FinancialInformation, model_path):
+
+def data(FinancialInformation: FinancialInformation, model_path):
     sample = pd.DataFrame({
-        "Age" : [FinancialInformation.Age],
-        "AnnualIncome" : [FinancialInformation.AnnualIncome],
-        "CreditScore" : [FinancialInformation.CreditScore],
-        "EmploymentStatus" : [FinancialInformation.EmploymentStatus],
-        "EducationLevel" : [FinancialInformation.EducationLevel],
-        "Experience" : [FinancialInformation.Experience],
-        "LoanAmount" : [FinancialInformation.LoanAmount],
-        "LoanDuration" : [FinancialInformation.LoanDuration],
-        "MaritalStatus" : [FinancialInformation.MaritalStatus],
-        "NumberOfDependents" : [FinancialInformation.NumberOfDependents],
-        "HomeOwnershipStatus" : [FinancialInformation.HomeOwnershipStatus],
-        "MonthlyDebtPayments" : [FinancialInformation.MonthlyDebtPayments],
-        "CreditCardUtilizationRate" : [FinancialInformation.CreditCardUtilizationRate],
-        "NumberOfOpenCreditLines" : [FinancialInformation.NumberOfOpenCreditLines],
-        "NumberOfCreditInquiries" : [FinancialInformation.NumberOfCreditInquiries],
-        "DebtToIncomeRatio" : [FinancialInformation.DebtToIncomeRatio],
-        "BankruptcyHistory" : [FinancialInformation.BankruptcyHistory],
-        "LoanPurpose" : [FinancialInformation.LoanPurpose],
-        "PreviousLoanDefaults" : [FinancialInformation.PreviousLoanDefaults],
-        "PaymentHistory" : [FinancialInformation.PaymentHistory],
-        "LengthOfCreditHistory" : [FinancialInformation.LengthOfCreditHistory],
-        "SavingsAccountBalance" : [FinancialInformation.SavingsAccountBalance],
-        "CheckingAccountBalance" : [FinancialInformation.CheckingAccountBalance],
-        "TotalAssets" : [FinancialInformation.TotalAssets],
-        "TotalLiabilities" : [FinancialInformation.TotalLiabilities],
-        "MonthlyIncome" : [FinancialInformation.MonthlyIncome],
-        "UtilityBillsPaymentHistory" : [FinancialInformation.UtilityBillsPaymentHistory],
-        "JobTenure" : [FinancialInformation.JobTenure],
-        "NetWorth" : [FinancialInformation.NetWorth],
-        "BaseInterestRate" : [FinancialInformation.BaseInterestRate],
-        "InterestRate" : [FinancialInformation.InterestRate],
-        "MonthlyLoanPayment" : [FinancialInformation.MonthlyLoanPayment],
-        "TotalDebtToIncomeRatio" : [FinancialInformation.TotalDebtToIncomeRatio]
+        "Age": [FinancialInformation.Age],
+        "AnnualIncome": [FinancialInformation.AnnualIncome],
+        "CreditScore": [FinancialInformation.CreditScore],
+        "EmploymentStatus": [FinancialInformation.EmploymentStatus],
+        "EducationLevel": [FinancialInformation.EducationLevel],
+        "Experience": [FinancialInformation.Experience],
+        "LoanAmount": [FinancialInformation.LoanAmount],
+        "LoanDuration": [FinancialInformation.LoanDuration],
+        "MaritalStatus": [FinancialInformation.MaritalStatus],
+        "NumberOfDependents": [FinancialInformation.NumberOfDependents],
+        "HomeOwnershipStatus": [FinancialInformation.HomeOwnershipStatus],
+        "MonthlyDebtPayments": [
+            FinancialInformation.MonthlyDebtPayments
+        ],
+        "CreditCardUtilizationRate": [
+            FinancialInformation.CreditCardUtilizationRate
+        ],
+        "NumberOfOpenCreditLines": [
+            FinancialInformation.NumberOfOpenCreditLines
+        ],
+        "NumberOfCreditInquiries": [
+            FinancialInformation.NumberOfCreditInquiries
+        ],
+        "DebtToIncomeRatio": [FinancialInformation.DebtToIncomeRatio],
+        "BankruptcyHistory": [FinancialInformation.BankruptcyHistory],
+        "LoanPurpose": [FinancialInformation.LoanPurpose],
+        "PreviousLoanDefaults": [FinancialInformation.PreviousLoanDefaults],
+        "PaymentHistory": [FinancialInformation.PaymentHistory],
+        "LengthOfCreditHistory": [FinancialInformation.LengthOfCreditHistory],
+        "SavingsAccountBalance": [FinancialInformation.SavingsAccountBalance],
+        "CheckingAccountBalance": [
+            FinancialInformation.CheckingAccountBalance
+        ],
+        "TotalAssets": [FinancialInformation.TotalAssets],
+        "TotalLiabilities": [
+            FinancialInformation.TotalLiabilities
+        ],
+        "MonthlyIncome": [FinancialInformation.MonthlyIncome],
+        "UtilityBillsPaymentHistory": [
+            FinancialInformation.UtilityBillsPaymentHistory
+        ],
+        "JobTenure": [FinancialInformation.JobTenure],
+        "NetWorth": [FinancialInformation.NetWorth],
+        "BaseInterestRate": [FinancialInformation.BaseInterestRate],
+        "InterestRate": [FinancialInformation.InterestRate],
+        "MonthlyLoanPayment": [FinancialInformation.MonthlyLoanPayment],
+        "TotalDebtToIncomeRatio": [FinancialInformation.TotalDebtToIncomeRatio]
     })
 
     sample = dp.feature_engineering(sample)
@@ -58,7 +72,7 @@ def data(FinancialInformation : FinancialInformation, model_path):
     return sample
 
 
-def model_load(path : str):
+def model_load(path: str):
     ensemble_model = joblib.load(path)
     return ensemble_model
 
@@ -69,16 +83,9 @@ def index():
 
 
 @app.post("/prediction")
-def model_predict(FinancialInformation : FinancialInformation):
-    model_path = os.path.join(os.path.dirname(__file__), "..", "..","models")
+def model_predict(FinancialInformation: FinancialInformation):
+    model_path = os.path.join(os.path.dirname(__file__), "..", "..", "models")
     sample = data(FinancialInformation, model_path)
     model = model_load(os.path.join(model_path, "ensemble_model.pkl"))
     predicted_score = model.predict(sample)
     return np.round(predicted_score[0], 2)
-
-
-
-
-
-
-
